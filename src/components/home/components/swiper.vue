af39c1c8252921ed2140962b969ffc03871f0361
953708519 <39235365+953708519@users.noreply.github.com>
2018-09-21 17:28:27 +0800

    轮播

---
+++ src/components/home/components/swiper.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="wrap">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id"><img :src="item.imgUrl" class="swiper-img"></swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: ' .swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '01',
+          imgUrl: 'http://img62.ddimg.cn/upload_img/00087/geq/750x315_zl_0905-1537151444.jpg'
+        },
+        {
+          id: '02',
+          imgUrl: 'http://img52.ddimg.cn/9003330047303182.jpg'
+        },
+        {
+          id: '03',
+          imgUrl: 'http://img61.ddimg.cn/upload_img/00733/422zhsj/750-315-1526620804.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrap >>> .swiper-pagination-bullet-active
+  background-color #fff
+.wrap
+  width 100%
+  height 0
+  padding-bottom 42%
+  overflow hidden
+  .swiper-img
+     width 100%
+</style>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import VueAwesomeSwiper from 'vur-awesome-swiper'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/css/reset.min.css'
 import './assets/css/border.css'
 import './assets/css/iconfont.css'
 import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
